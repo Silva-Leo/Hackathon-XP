3b27019ae9e2c2bf17e57742ff9896c7bb3308b2
Mooteus <moteus10@gmail.com>
2022-02-20 16:14:43 -0300

    add Client List img

--- hacka-xp/src/components/Home/ListClients/listClient.js
+++ hacka-xp/src/components/Home/ListClients/listClient.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import Client from '../../../assets/img/home/listaClientes.svg';
 import './listClient.scss'
 
 export default function ListClient () {
     return (
         <>
             <div className='clientsContainer'>
-
+                <img src={Client} alt='listaClientes'></img>
             </div>
         </>
     )
