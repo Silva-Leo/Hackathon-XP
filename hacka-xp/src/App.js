4f5e9b48d1c8b010fc2bb68e50bc8903fe7b116c
Leo Silva <devleonardosilva@gmail.com>
2022-02-20 19:04:31 -0300

    Fix: Header links

--- hacka-xp/src/App.js
+++ hacka-xp/src/App.js
@@ -3,10 +3,6 @@ import Layout from './components/Layout/index.js';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 
-
-
-
-
 import Descobrir from './pages/Descobrir';
 
 import Home from './pages/Home'
@@ -19,18 +15,12 @@ function App() {
       <BrowserRouter>
         <Layout>
           <Routes>
-  
             <Route path="/enriquecer" element={<Enriquecer />} />
-
             <Route path="/descobrir" element={<Descobrir />} />
-           {/* <Route path="/acompanhar" element={<Acompanhar />} />
-
             <Route path="" element={<Home />} />
-            {/* <Route path="/descobrir" element={<Descobrir />} />
-            <Route path="/acompanhar" element={<Acompanhar />} />
+            {/* <Route path="/acompanhar" element={<Acompanhar />} />
+            <Route path="/historico" element={<Historico />} /> */}
 
-            <Route path="/historico" element={<Historico />} />
-             */}
           </Routes>
         </Layout>
       </BrowserRouter>
