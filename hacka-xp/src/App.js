16a8e27eb61baacdd33326984df3149137bf00ee
Leo Silva <devleonardosilva@gmail.com>
2022-02-20 13:11:14 -0300

    Fix: Footer

--- hacka-xp/src/App.js
+++ hacka-xp/src/App.js
@@ -2,18 +2,25 @@ import './App.css';
 import Layout from './components/Layout/index.js';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
+
+
+
+
+
+import Enriquecer from './pages/Enriquecimento';
+
 function App() {
   return (
     <>
       <BrowserRouter>
         <Layout>
           <Routes>
-            <Route path="/" element={<Home />} />
+  
             <Route path="/enriquecer" element={<Enriquecer />} />
-            <Route path="/descobrir" element={<Descobrir />} />
+            {/* <Route path="/descobrir" element={<Descobrir />} />
             <Route path="/acompanhar" element={<Acompanhar />} />
             <Route path="/historico" element={<Historico />} />
-            <Route path="" element={<Home />} />
+            <Route path="" element={<Home />} /> */}
           </Routes>
         </Layout>
       </BrowserRouter>
